4a269158717d66012c95ed334cbfe795b28662a2
Assafm <assafe.cohen@gmail.com>
2019-12-08 19:39:08 +0200

    changed react-bootstrap/button to react-bootstrap/Button

--- src/components/ModalDelete/ModalDelete.js
+++ src/components/ModalDelete/ModalDelete.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import Button from 'react-bootstrap/button'
-import Modal from 'react-bootstrap/modal'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 
 const ModalDelete = (props) => {
